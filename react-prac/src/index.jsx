2a3031eb48f06bb5fe06f517be27c10910bfa9c3
carternadain <cnadain@gmail.com>
2025-07-01 22:36:50 -0700

    update init

---
+++ react-prac/src/index.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from "react-dom/client"
+
+
+function MyCompenent (){
+    return(
+        <h1>
+            React is so cool
+        </h1>
+    )
+}
+
+export default MyCompenent

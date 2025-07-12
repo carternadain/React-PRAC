29106ee386097c17718034b95ada1a3341d133b7
carternadain <cnadain@gmail.com>
2025-07-12 13:22:12 -0700

    custom comp

--- react-prac/src/index.jsx
+++ react-prac/src/index.jsx
@@ -1,30 +1,25 @@
-import {createRoot} from "react-dom/client"
+import { createRoot } from "react-dom/client";
 
-function staticPage (){
-    return (
-        <section>
-            <img src="src/assets/react.svg"/>
-            <h1> hello! </h1>
-        </section>
-      
+
+function Test () {
+   return (
+        <div>
+            <ul>
+                <li>
+                    hello
+                </li>
+                <li>
+                    hello
+                </li>
+                <li>
+                    hello
+                </li>
+                <li>
+                    hello
+                </li>
+            </ul>
+        </div>
     )
 }
 
-export default staticPage 
-
-
-/*
-Challenge: Starting from scratch, build and render the 
-HTML for our section project. Check the Google slide for 
-what you're trying to build.
-
-We'll be adding more styling to it later.
-
-Hints:
-* The React logo is a file in the project tree, so you can
-  access it by using `src="react-logo.png"` in your image
-  element
-* You can also set the `width` attribute of the image element
-  just like in HTML. In the slide, I have it set to 40px
- */
- 
+export default Test

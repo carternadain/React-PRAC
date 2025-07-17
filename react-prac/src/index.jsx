e5e824dac071be6706c597b3582190b98b8d4162
carternadain <cnadain@gmail.com>
2025-07-16 22:44:13 -0700

    update

--- react-prac/src/index.jsx
+++ react-prac/src/index.jsx
@@ -1,25 +1,28 @@
 import { createRoot } from "react-dom/client";
 
 
-function Test () {
-   return (
-        <div>
-            <ul>
-                <li>
-                    hello
-                </li>
-                <li>
-                    hello
-                </li>
-                <li>
-                    hello
-                </li>
-                <li>
-                    hello
-                </li>
-            </ul>
-        </div>
+// ////
+// - Add a `<header>` element with an `<img />` element with the image of the 
+//   React logo inside (src="react-logo.png") and make sure to set the 
+//   width to something more manageable so it doesn't take up the whole screen.
+//   Also, as always, you should include some alt text on the image.
+// - Add an `<h1>` with some text describing the page. (E.g. "Reasons
+//   I'm excited to learn React"). Place it above the ordered list, then wrap
+//   the `<h1>` and `<ol>` inside a `<main>` element to keep our semantic
+//   structure flowing well.
+// - Add a `<footer>` after the list that says: 
+//     "© 20xx <last name here> development. All rights reserved."
+//  */////
+
+
+function TestFunc () {
+  return  (
+        <header> <img src="" alt="react logo" /> 
+        <section> <h1>React facts! </h1> </section>
+       </header>
+  
+       
     )
 }
 
-export default Test
+export default TestFunc 
